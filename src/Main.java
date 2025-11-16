3332467d025ca83e3bfbfb1ad102816031fe029f
NurgalyAshkenov <nurikaskenov147@gmail.com>
2025-11-16 20:22:49 +0500

    main

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,32 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+package com.vehiclerental;
+
+import com.vehiclerental.abstractfactory.*;
+import com.vehiclerental.data.VehicleInventory;
+import com.vehiclerental.facade.RentalServiceFacade;
+import com.vehiclerental.factory.Vehicle;
+import com.vehiclerental.observer.Customer;
+import com.vehiclerental.observer.VehicleAvailabilityNotifier;
+import com.vehiclerental.strategy.*;
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
-    }
-}
+        Scanner sc = new Scanner(System.in);
+
+        VehicleAbstractFactory petrolFactory = new PetrolVehicleFactory();
+        VehicleAbstractFactory electricFactory = new ElectricVehicleFactory();
+
+        VehicleInventory inventory = new VehicleInventory();
+        VehicleAvailabilityNotifier notifier = new VehicleAvailabilityNotifier();
+        RentalServiceFacade service = new RentalServiceFacade(inventory, notifier);
+
+        inventory.addVehicle(petrolFactory.createCar1()); // Sedan
+        inventory.addVehicle(petrolFactory.createCar2()); // Cobalt
+        inventory.addVehicle(petrolFactory.createCar3()); // Lexux
+        inventory.addVehicle(petrolFactory.createCar4()); // Land Cruizer
+
+        inventory.addVehicle(electricFactory.createCar1()); // BYD
+        inventory.addVehicle(electricFactory.createCar2()); // Chery
+        inventory.addVehicle(electricFactory.createCar3()); // Zeekr
+        inventory.addVehicle(electricFactory.createCar4()); // Lixiang
